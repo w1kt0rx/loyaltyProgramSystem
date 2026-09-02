5468974abb6987dd25b1db16bda2fe38a4f2ed1e
w1kt0rx <szarekwiktor13@gmail.com>
2026-09-02 11:24:40 +0200

    Revert "Feature/zad 02"

--- src/main/java/com/example/loyaltyprogram/exception/LoyaltyProgramSystemException.java
+++
@@ -1,7 +0,0 @@
-package com.example.loyaltyprogram.exception;
-
-public class LoyaltyProgramSystemException extends RuntimeException {
-    public LoyaltyProgramSystemException(String message) {
-        super(message);
-    }
-}
