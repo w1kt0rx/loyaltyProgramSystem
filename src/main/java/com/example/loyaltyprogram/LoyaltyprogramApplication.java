a1a71a76fc1b3ac26850eb5d0e5e5266bc9b588e
szare <szarekwiktor13@gmail.com>
2026-09-02 09:59:42 +0200

    feature/ZAD-01 basic structure created

---
+++ src/main/java/com/example/loyaltyprogram/LoyaltyprogramApplication.java
@@ -0,0 +1,13 @@
+package com.example.loyaltyprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyprogramApplication.class, args);
+	}
+
+}
